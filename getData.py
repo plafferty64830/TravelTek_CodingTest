c205c72e3db6ac44dcfafd036120418d4b54a17d
plafferty64830 <plafferty64830@gmail.com>
2020-09-25 17:38:16 +0100

    uncommented required code from getData.py and adjusted 'Purpose'

--- getData.py
+++ getData.py
@@ -6,9 +6,7 @@ Created on Mon Sep 21 09:29:32 2020
 
 Purpose: 
 
-Retrieves data from a CSV file (aka Database in a real life scenario), 
-Converts this data to JSON and returns the JSON object to the web frontend
-using an HTTP GET request.
+Retrieves data from a CSV file (aka Database in a real life scenario) and sends to MongoDB
 
 """
 
@@ -16,22 +14,19 @@ import csv
 import pymongo
 from datetime import datetime
 
-
-print("Start Time =", datetime.now())
-
 mgClient  = pymongo.MongoClient("mongodb+srv://plafferty:Polopolo1@cluster0.ksqru.gcp.mongodb.net/flightDB?retryWrites=true&w=majority")
 flightDB  = mgClient['flightDB']
-#flightCol = flightDB['flights']
-#segCol    = flightDB['segments']
-#iataCol   = flightDB['iata']
+flightCol = flightDB['flights']
+segCol    = flightDB['segments']
+iataCol   = flightDB['iata']
 aLineCol   = flightDB['airlines']
 
-#mainFlData = open('D:/TravelTek/flighdata_B.csv', 'r',encoding="utf-8")
-#segFData = open('D:/TravelTek/flighdata_B_segments.csv','r',encoding="utf-8")
-#iataFData = open('D:/TravelTek/iataCodes.csv','r',encoding="utf-8")
+mainFlData = open('D:/TravelTek/flighdata_B.csv', 'r',encoding="utf-8")
+segFData = open('D:/TravelTek/flighdata_B_segments.csv','r',encoding="utf-8")
+iataFData = open('D:/TravelTek/iataCodes.csv','r',encoding="utf-8")
 alineFData = open('D:/TravelTek/airlineImg.csv','r',encoding="utf-8")
 
-"""
+
 mainFlHead = ["id","depair","destair","indepartcode","inarrivecode",
               "outflightno","outdepartdate","outdeparttime","outarrivaldate",
               "outarrivaltime","outbookingclass","outflightclass","outcarriercode",
@@ -48,17 +43,17 @@ segFHead = ["flightid","depcode","arrcode","depdate","arrdate","deptime",
 
 iataFHead = ['name','code']
 
-"""
+
 alineFHead = ['name','imgName', 'natHeight','natWidth']
 
 #read files into a variable
-#readMain = csv.DictReader(mainFlData,mainFlHead)
-#readSeg  = csv.DictReader(segFData,segFHead)
-#readIata = csv.DictReader(iataFData,iataFHead)
+readMain = csv.DictReader(mainFlData,mainFlHead)
+readSeg  = csv.DictReader(segFData,segFHead)
+readIata = csv.DictReader(iataFData,iataFHead)
 readALine = csv.DictReader(alineFData,alineFHead)
 
 #send the flight data to MongoDB
-"""
+
 for mainRow in readMain:
     flightCol.insert_one(mainRow) 
     
@@ -66,22 +61,17 @@ for mainRow in readMain:
 #send the segment data to MongoDB  
 for segRow in readSeg:
     segCol.insert_one(segRow) 
+	
+#send iata data to MongoDB
+for iataRow in readIata:
+    iataCol.insert_one(iataRow)
+	
+for aLineRow in readALine:
+    aLineCol.insert_one(aLineRow)
 
 
 #close input streams
 mainFlData.close()
 segFData.close()
 iataFData.close()
-
-#send iata data to MongoDB
-for iataRow in readIata:
-    iataCol.insert_one(iataRow)
-    
-"""
-
-for aLineRow in readALine:
-    aLineCol.insert_one(aLineRow)
-
 alineFData.close()
-
-print("Finish Time =", datetime.now())
